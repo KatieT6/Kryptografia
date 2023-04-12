ebfa0930dba3a90d42423149aad2e6bbcc6950d7
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2023-04-12 21:29:51 +0200

    DES en/decryption

--- 3DES/3DES/DesAlgorithm.cs
+++ 3DES/3DES/DesAlgorithm.cs
@@ -9,28 +9,190 @@ namespace _3DES
     public class DesAlgorithm
     {
 
-        //Pierwsza permutacja bloku z IPArray
-        public static byte[] InitialPermutation(byte[] input)
-        {
-            byte[] output = new byte[8];
+        #region Helper methods
 
-            // Permute the input block according to the IP table
-            for (int i = 0; i < 64; i++)
+        // Helper method to expand a 32-bit value to a 48-bit value using the E table
+        private static byte[] Expand(byte[] value)
+        {
+            byte[] expanded = new byte[6];
+
+            for (int i = 0; i < 48; i++)
             {
-                int bitIndex = Arrays.IPArray[i] - 1;  // The bit index in the input block
-                int byteIndex = bitIndex / 8;   // The byte index in the input block
+                int bitIndex = Arrays.E_bit_selectionArrays[i] - 1;   // The bit index in the expanded value
+                int byteIndex = bitIndex / 8;   // The byte index in the original value
                 int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
 
-                // If the bit is set in the input block, set it in the output block as well
-                if ((input[byteIndex] & bitMask) != 0)
+                // If the bit is set in the original value, set it in the expanded value as well
+                if ((value[byteIndex] & bitMask) != 0)
                 {
-                    int outputByteIndex = i / 8;
-                    int outputBitIndex = i % 8;
-                    output[outputByteIndex] |= (byte)(1 << outputBitIndex);
+                    int expandedByteIndex = i / 8;
+                    int expandedBitIndex = i % 8;
+                    expanded[expandedByteIndex] |= (byte)(1 << expandedBitIndex);
                 }
             }
 
-            return output;
+            return expanded;
         }
+
+
+        // Helper method to XOR two byte arrays
+        private static byte[] Xor(byte[] a, byte[] b)
+        {
+            byte[] result = new byte[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                result[i] = (byte)(a[i] ^ b[i]);
+            }
+            return result;
+        }
+
+        // Helper method to apply the S-boxes to a 48-bit value
+        private static byte[] SBox(byte[] value)
+        {
+            byte[] sboxed = new byte[4];
+            for (int i = 0; i < 8; i++)
+            {
+                // Extract the 6-bit value for this S-box
+                int sboxInput = 0;
+                for (int j = 0; j < 6; j++)
+                {
+                    int bitIndex = i * 6 + j;
+                    int byteIndex = bitIndex / 8;
+                    int bitMask = 1 << (bitIndex % 8);
+                    if ((value[byteIndex] & bitMask) != 0)
+                    {
+                        sboxInput |= (1 << (5 - j));
+                    }
+                }
+
+                // Apply the S-box and store the result in the sboxed array
+                int sboxOutput = Arrays.S_boxArrays[i, sboxInput];
+                for (int j = 0; j < 4; j++)
+                {
+                    int bitIndex = i * 4 + j;
+                    int byteIndex = bitIndex / 8;
+                    int bitMask = 1 << (bitIndex % 8);
+                    if ((sboxOutput & (1 << (3 - j))) != 0)
+                    {
+                        sboxed[byteIndex] |= (byte)bitMask;
+                    }
+                }
+            }
+            return sboxed;
+        }
+
+        #endregion
+
+        public static byte[] Encrypt(byte[] permutedMessage, byte[][] subkeys)
+        {
+            byte[] l = new byte[4];
+            byte[] r = new byte[4];
+            Array.Copy(permutedMessage, 0, l, 0, 4);
+            Array.Copy(permutedMessage, 4, r, 0, 4);
+
+            // Perform 16 rounds of encryption
+            for (int i = 0; i < 16; i++)
+            {
+                // Save the previous values of L and R
+                byte[] previousL = l;
+                byte[] previousR = r;
+
+                // Compute the new value of R using the expansion table, the subkey, and the S-boxes
+                byte[] expanded = Expand(r);
+                byte[] subkey = subkeys[i];
+                byte[] xored = Xor(expanded, subkey);
+                byte[] sboxed = SBox(xored);
+                byte[] permutedSboxed = Permutation.Permute(sboxed, Arrays.S_boxPermutation);
+
+                // Compute the new value of L by XORing it with the permuted and S-boxed value of R
+                l = previousR;
+                r = Xor(previousL, permutedSboxed);
+            }
+
+            // Concatenate the final values of L and R and permute them using the IP^-1 table
+            byte[] combined = new byte[8];
+            Array.Copy(r, 0, combined, 0, 4);
+            Array.Copy(l, 0, combined, 4, 4);
+
+            byte[] encrypted = new byte[8];
+            for (int i = 0; i < 64; i++)
+            {
+                int bitIndex = Arrays.IPInverseArray[i] - 1;  // The bit index in the combined message
+                int byteIndex = bitIndex / 8;   // The byte index in the combined message
+                int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
+
+                // If the bit is set in the combined message, set it in the encrypted message as well
+                if ((combined[byteIndex] & bitMask) != 0)
+                {
+                    int encryptedByteIndex = i / 8;
+                    int encryptedBitIndex = i % 8;
+                    encrypted[encryptedByteIndex] |= (byte)(1 << encryptedBitIndex);
+                }
+            }
+
+            return encrypted;
+        }
+
+        public static byte[] Decrypt(byte[] encryptedMessage, byte[][] subkeys)
+        {
+            // Permute the encrypted message using the initial permutation table
+            byte[] permutedMessage = Permutation.Permute(encryptedMessage, Arrays.IPArray);
+
+            // Split the permuted message into L and R halves
+            byte[] l = new byte[4];
+            byte[] r = new byte[4];
+            Array.Copy(permutedMessage, 0, l, 0, 4);
+            Array.Copy(permutedMessage, 4, r, 0, 4);
+
+            // Perform 16 rounds of decryption in reverse order
+            for (int i = 15; i >= 0; i--)
+            {
+                // Save the previous values of L and R
+                byte[] previousL = l;
+                byte[] previousR = r;
+
+                // Compute the new value of L using the permuted and S-boxed value of R
+                byte[] expanded = Expand(previousL);
+                byte[] subkey = subkeys[i];
+                byte[] xored = Xor(expanded, subkey);
+                byte[] sboxed = SBox(xored);
+                byte[] permutedSboxed = Permutation.Permute(sboxed, Arrays.S_boxPermutation);
+                l = Xor(previousR, permutedSboxed);
+
+                // Compute the new value of R by XORing it with the permuted and S-boxed value of L
+                r = previousL;
+            }
+
+            // Concatenate the final values of L and R and permute them using the IP^-1 table
+            byte[] combined = new byte[8];
+            Array.Copy(r, 0, combined, 0, 4);
+            Array.Copy(l, 0, combined, 4, 4);
+
+            byte[] decrypted = new byte[8];
+            for (int i = 0; i < 64; i++)
+            {
+                int bitIndex = Arrays.IPInverseArray[i] - 1;  // The bit index in the combined message
+                int byteIndex = bitIndex / 8;   // The byte index in the combined message
+                int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
+
+                // If the bit is set in the combined message, set it in the decrypted message as well
+                if ((combined[byteIndex] & bitMask) != 0)
+                {
+                    int decryptedByteIndex = i / 8;
+                    int decryptedBitIndex = i % 8;
+                    decrypted[decryptedByteIndex] |= (byte)(1 << decryptedBitIndex);
+                }
+            }
+
+            return decrypted;
+        }
+
+
+
+
+
+
+
     }
+
 }
--- 3DES/3DES/Key.cs
+++ 3DES/3DES/Key.cs
@@ -50,92 +50,160 @@ namespace _3DES
             return test;
         }
 
-
-        public static byte[][] GenerateSubkeys(byte[] key)
+        private static byte[][] GenerateSubKeys(byte[] key)
         {
-            byte[] permutedKey = new byte[7];
-            for (int i = 0; i < 56; i++)
-            {
-                int bitIndex = PC_Arrays.PC1[i] - 1;  // The bit index in the input key
-                int byteIndex = bitIndex / 8;   // The byte index in the input key
-                int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
+            // Permute the key using the PC-1 table
+            byte[] permutedKey = Permutation.Permute(key, PC_Arrays.PC1);
 
-                // If the bit is set in the input key, set it in the permuted key as well
-                if ((key[byteIndex] & bitMask) != 0)
-                {
-                    int permutedByteIndex = i / 8;
-                    int permutedBitIndex = i % 8;
-                    permutedKey[permutedByteIndex] |= (byte)(1 << permutedBitIndex);
-                }
-            }
-
-            // Split the 56-bit intermediate key into two 28-bit halves
+            // Split the permuted key into two 28-bit halves
             byte[] c = new byte[4];
             byte[] d = new byte[4];
-            Array.Copy(permutedKey, 0, c, 0, 4);
-            Array.Copy(permutedKey, 4, d, 0, 4);
-
-            byte[][] subkeys = new byte[16][];
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < 28; i++)
             {
-                // Shift the two halves of the key according to the shift schedule
-                int shiftCount = PC_Arrays.Shifts[i];
-                
-                //jak nei zadziałą to zmienićfunkcję Shifts tak zeby zwrracała nowe wartości 
-                ShiftLeft(c, shiftCount);
-                ShiftLeft(d, shiftCount);
-
-                // Combine the two halves and permute the result according to the PC2 table
-                byte[] combined = new byte[7];
-                Array.Copy(c, 0, combined, 0, 4);
-                Array.Copy(d, 0, combined, 4, 4);
-
-                byte[] subkey = new byte[6];
-                for (int j = 0; j < 48; j++)
+                int bitIndex = PC_Arrays.PC1[i] - 1;
+                int byteIndex = bitIndex / 8;
+                int bitMask = 1 << (bitIndex % 8);
+                if ((permutedKey[byteIndex] & bitMask) != 0)
                 {
-                    int bitIndex = PC_Arrays.PC2[j] - 1;  // The bit index in the combined key
-                    int byteIndex = bitIndex / 8;   // The byte index in the combined key
-                    int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
-
-                    // If the bit is set in the the combined key, set it in the subkey as well
-                    if ((combined[byteIndex] & bitMask) != 0)
+                    if (i < 28)
                     {
-                        int subkeyByteIndex = j / 8;
-                        int subkeyBitIndex = j % 8;
-                        subkey[subkeyByteIndex] |= (byte)(1 << subkeyBitIndex);
+                        c[byteIndex] |= (byte)(1 << (i % 8));
+                    }
+                    else
+                    {
+                        d[byteIndex] |= (byte)(1 << (i % 8));
                     }
                 }
-
-                subkeys[i] = subkey;
             }
 
-            return subkeys;
+            // Generate the 16 subkeys by rotating the halves and permuting using the PC-2 table
+            byte[][] subKeys = new byte[16][];
+            for (int i = 0; i < 16; i++)
+            {
+                // Rotate the halves by the appropriate amount
+                int rotateAmount = PC_Arrays.Shifts[i];
+                byte[] cRotated = RotateLeft(c, rotateAmount);
+                byte[] dRotated = RotateLeft(d, rotateAmount);
+
+                // Concatenate the rotated halves and permute using the PC-2 table
+                byte[] cd = Concatenate(cRotated, dRotated);
+                subKeys[i] = Permutation.Permute(cd, PC_Arrays.PC2);
+
+                c = cRotated;
+                d = dRotated;
+            }
+
+            return subKeys;
         }
 
-
-        // Helper method to shift the bits in an array of bytes to the left by a given count
-        private static void ShiftLeft(byte[] array, int count)
+        // Helper method to rotate a byte array to the left by a given number of bits
+        private static byte[] RotateLeft(byte[] value, int amount)
         {
-            // Calculate the number of whole bytes to shift
-            int byteCount = count / 8;
-            int bitCount = count % 8;
-
-            // Shift the whole bytes
-            for (int i = 0; i < array.Length - byteCount; i++)
+            byte[] rotated = new byte[value.Length];
+            for (int i = 0; i < value.Length; i++)
             {
-                array[i] = array[i + byteCount];
+                int leftShift = amount % 8;
+                int rightShift = 8 - leftShift;
+                rotated[i] = (byte)(((value[i] << leftShift) & 0xFF) | ((value[(i + 1) % value.Length] >> rightShift) & 0xFF));
             }
-
-            // Shift the remaining bits in the last byte
-            byte lastByte = array[array.Length - byteCount - 1];
-            for (int i = 0; i < bitCount; i++)
-            {
-                lastByte <<= 1;
-                lastByte |= (byte)((lastByte >> 8) & 1);
-            }
-            array[array.Length - 1] = lastByte;
+            return rotated;
         }
 
+        // Helper method to concatenate two byte arrays
+        private static byte[] Concatenate(byte[] a, byte[] b)
+        {
+            byte[] concatenated = new byte[a.Length + b.Length];
+            Array.Copy(a, 0, concatenated, 0, a.Length);
+            Array.Copy(b, 0, concatenated, a.Length, b.Length);
+            return concatenated;
+        }
 
-     }
+        //pierwsza próba 
+        /* public static byte[][] GenerateSubkeys(byte[] key)
+         {
+             byte[] permutedKey = new byte[7];
+             for (int i = 0; i < 56; i++)
+             {
+                 int bitIndex = PC_Arrays.PC1[i] - 1;  // The bit index in the input key
+                 int byteIndex = bitIndex / 8;   // The byte index in the input key
+                 int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
+
+                 // If the bit is set in the input key, set it in the permuted key as well
+                 if ((key[byteIndex] & bitMask) != 0)
+                 {
+                     int permutedByteIndex = i / 8;
+                     int permutedBitIndex = i % 8;
+                     permutedKey[permutedByteIndex] |= (byte)(1 << permutedBitIndex);
+                 }
+             }
+
+             // Split the 56-bit intermediate key into two 28-bit halves
+             byte[] c = new byte[4];
+             byte[] d = new byte[4];
+             Array.Copy(permutedKey, 0, c, 0, 4);
+             Array.Copy(permutedKey, 4, d, 0, 4);
+
+             byte[][] subkeys = new byte[16][];
+             for (int i = 0; i < 16; i++)
+             {
+                 // Shift the two halves of the key according to the shift schedule
+                 int shiftCount = c;
+
+                 //jak nei zadział to zmienićfunkcję Shifts tak zeby zwrracała nowe wartości 
+                 ShiftLeft(c, shiftCount);
+                 ShiftLeft(d, shiftCount);
+
+                 // Combine the two halves and permute the result according to the PC2 table
+                 byte[] combined = new byte[7];
+                 Array.Copy(c, 0, combined, 0, 4);
+                 Array.Copy(d, 0, combined, 4, 4);
+
+                 byte[] subkey = new byte[6];
+                 for (int j = 0; j < 48; j++)
+                 {
+                     int bitIndex = PC_Arrays.PC2[j] - 1;  // The bit index in the combined key
+                     int byteIndex = bitIndex / 8;   // The byte index in the combined key
+                     int bitMask = 1 << (bitIndex % 8); // The bit mask for the corresponding bit
+
+                     // If the bit is set in the the combined key, set it in the subkey as well
+                     if ((combined[byteIndex] & bitMask) != 0)
+                     {
+                         int subkeyByteIndex = j / 8;
+                         int subkeyBitIndex = j % 8;
+                         subkey[subkeyByteIndex] |= (byte)(1 << subkeyBitIndex);
+                     }
+                 }
+
+                 subkeys[i] = subkey;
+             }
+
+             return subkeys;
+         }
+
+
+         // Helper method to shift the bits in an array of bytes to the left by a given count
+         private static void ShiftLeft(byte[] array, int count)
+         {
+             // Calculate the number of whole bytes to shift
+             int byteCount = count / 8;
+             int bitCount = count % 8;
+
+             // Shift the whole bytes
+             for (int i = 0; i < array.Length - byteCount; i++)
+             {
+                 array[i] = array[i + byteCount];
+             }
+
+             // Shift the remaining bits in the last byte
+             byte lastByte = array[array.Length - byteCount - 1];
+             for (int i = 0; i < bitCount; i++)
+             {
+                 lastByte <<= 1;
+                 lastByte |= (byte)((lastByte >> 8) & 1);
+             }
+             array[array.Length - 1] = lastByte;
+         }*/
+
+
+    }
 }
